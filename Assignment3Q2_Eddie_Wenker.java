6ecedcf6d2b9954b0238f387c1f438b9e5981393
Eddie Wenker <wenkere2@winthrop.edu>
2026-02-21 15:37:01 -0500

    Question 2 file

---
+++ Assignment3Q2_Eddie_Wenker.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Assignment3Q2_Eddie_Wenker {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s = input.nextLine();
+        System.out.print("Enter a character: ");
+        char c = input.next().charAt(0);
+        input.close();
+
+        int Occurrences = OccurrencesCount(s, c);
+        System.out.println( c + " occurs " + Occurrences + " times in \"" + s);
+    }
+    
+    public static int OccurrencesCount(String str, char c) {
+        // Base case: if the string is empty, return 0
+        if (str.isEmpty()) {
+            return 0;
+        }
+        // Recursive case: check the first character and add the count from the rest of the string
+        int count = (str.charAt(0) == c) ? 1 : 0;
+        return count + OccurrencesCount(str.substring(1), c);
+    }
+
+    public static String recursiveReverse(String str) {
+        // Base case: if the string is empty, return an empty string
+        if (str.isEmpty()) {
+            return "";
+        }
+        // Recursive case: return the last character + reverse of the substring excluding the last character
+        return str.charAt(str.length() - 1) + recursiveReverse(str.substring(0, str.length() - 1));
+    }
+}//Calculate Running Time!
